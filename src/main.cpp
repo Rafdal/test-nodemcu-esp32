f18be57fe1a04281de0f3d74e0ffb1cd9df68c7e
Rafael Dalzotto <rafaeldalzotto2016@gmail.com>
2020-07-20 00:49:32 -0300

    WebSockets are workinggit status (With Lambda functions too)

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,26 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <private_data.h>
+#include <private_data.h> // Aqui tengo passwords de red
 
-//Your Domain name with URL path or IP address with path
-const char *serverName = "http://192.168.1.114:3000/";
+#define USE_SERIAL Serial
 
-// the following variables are unsigned longs because the time, measured in
-// milliseconds, will quickly become a bigger number than can be stored in an int.
+const char *host = "192.168.1.114"; 
+const int port = 3000;
+
 unsigned long lastTime = 0;
-// Timer set to 10 minutes (600000)
-// unsigned long timerDelay = 600000;
-// Set timer to 5 seconds (5000)
 unsigned long timerDelay = 15000;
 
 // #include <google-get-request.h>
+
+
+#include <SocketIoClient.h>
+SocketIoClient webSocket;
 
 void setup()
 {
 	Serial.begin(576000);
 
-	 WiFi.mode(WIFI_STA);
+	WiFi.mode(WIFI_STA);
 	WiFi.begin(ssid, password);
 	Serial.println(F("Connecting"));
 	while (WiFi.status() != WL_CONNECTED)
@@ -32,10 +33,17 @@ void setup()
 
 	Serial.println(F("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading."));
 	lastTime = millis();
+
+	webSocket.on("chat:escribiendo", [](const char * payload, size_t length){
+		// ! Expresiones lambda en C++
+		Serial.printf("Recibido payload: %s\n", payload);
+	});
+    webSocket.begin(host, port);
 }
 
 void loop()
 {
+	webSocket.loop();
 	//Send an HTTP POST request every 10 minutes
 	if ((millis() - lastTime) > timerDelay)
 	{
